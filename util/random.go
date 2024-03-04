ff4487aa3ca7461a8bbdb3f196417de81951c1c9
Realman78 <marin.dedic@me.com>
2024-03-04 13:24:45 +0100

    init commit

---
+++ util/random.go
@@ -0,0 +1,44 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "qwertzuioplkjhgfdsayxcvbnm"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	if n == 0 {
+		return ""
+	}
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(4)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"EUR", "HRK", "USD", "KM"}
+	return currencies[rand.Intn(len(currencies))]
+}
