26957450dd2561edae1c992a2e8e7af695ad5836
Realman78 <marin.dedic@me.com>
2024-03-14 09:39:13 +0100

    Login added

--- api/user.go
+++ api/user.go
@@ -17,12 +17,22 @@ type createUserRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 }
 
-type createUserResponse struct {
+type userResponse struct {
 	Username          string    `json:"username"`
 	FullName          string    `json:"full_name"`
 	Email             string    `json:"email"`
 	PasswordChangedAt time.Time `json:"password_changed_at"`
 	CreatedAt         time.Time `json:"created_at"`
+}
+
+func newUserResponse(user db.User) userResponse {
+	return userResponse{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt:         user.CreatedAt,
+	}
 }
 
 // Gin koristi validator library under the hood, samo dodas "binding: required"
@@ -57,12 +67,88 @@ func (server *Server) createUser(ctx *gin.Context) {
 		return
 	}
 
-	res := createUserResponse{
-		Username:          user.Username,
-		FullName:          user.FullName,
-		Email:             user.Email,
-		PasswordChangedAt: user.PasswordChangedAt,
-		CreatedAt:         user.CreatedAt,
-	}
+	res := newUserResponse(user)
 	ctx.JSON(http.StatusOK, res)
+}
+
+type loginUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
+type loginUserResponse struct {
+	// SessionID             uuid.UUID    `json:"session_id"`
+	AccessToken string `json:"access_token"`
+	// AccessTokenExpiresAt  time.Time    `json:"access_token_expires_at"`
+	// RefreshToken          string       `json:"refresh_token"`
+	// RefreshTokenExpiresAt time.Time    `json:"refresh_token_expires_at"`
+	User userResponse `json:"user"`
+}
+
+func (server *Server) loginUser(ctx *gin.Context) {
+	var req loginUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		// if errors.Is(err, db.ErrRecordNotFound) {
+		// 	ctx.JSON(http.StatusNotFound, errorResponse(err))
+		// 	return
+		// }
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	err = util.CheckPassword(req.Password, user.HashedPassword)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	accessToken, _, err := server.tokenMaker.CreateToken(
+		user.Username,
+		// user.Role,
+		server.config.AccessTokenDuration,
+	)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(
+	// 	user.Username,
+	// 	// user.Role,
+	// 	server.config.RefreshTokenDuration,
+	// )
+	// if err != nil {
+	// 	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	// 	return
+	// }
+
+	// session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
+	// 	ID:           refreshPayload.ID,
+	// 	Username:     user.Username,
+	// 	RefreshToken: refreshToken,
+	// 	UserAgent:    ctx.Request.UserAgent(),
+	// 	ClientIp:     ctx.ClientIP(),
+	// 	IsBlocked:    false,
+	// 	ExpiresAt:    refreshPayload.ExpiredAt,
+	// })
+	// if err != nil {
+	// 	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	// 	return
+	// }
+
+	rsp := loginUserResponse{
+		// SessionID:             session.ID,
+		AccessToken: accessToken,
+		// AccessTokenExpiresAt:  accessPayload.ExpiredAt,
+		// RefreshToken:          refreshToken,
+		// RefreshTokenExpiresAt: refreshPayload.ExpiredAt,
+		User: newUserResponse(user),
+	}
+	ctx.JSON(http.StatusOK, rsp)
 }
