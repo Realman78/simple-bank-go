77a6e1cc8f800e07f0d2de8388d6a0e30e9dc7b8
Realman78 <marin.dedic@me.com>
2024-03-21 03:01:36 +0100

    Added grpc configuration

---
+++ gapi/server.go
@@ -0,0 +1,38 @@
+package gapi
+
+import (
+	"fmt"
+
+	db "simplebank/db/sqlc"
+	"simplebank/pb"
+	"simplebank/token"
+	"simplebank/util"
+	// "simplebank/worker"
+)
+
+// Server serves gRPC requests for our banking service.
+type Server struct {
+	pb.UnimplementedSimpleBankServer // ovo se doda da bi server mogal prihvacat
+	// zahtjeve na neke RPC-ove bez da su jos implementirani (samo ce hitit ovaj unimplemented error)
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	// taskDistributor worker.TaskDistributor
+}
+
+// NewServer creates a new gRPC server.
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+		// taskDistributor: taskDistributor,
+	}
+
+	return server, nil
+}
